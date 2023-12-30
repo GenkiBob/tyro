2e988833201a0bfc7ff936acc8aca779381008b7
panny <panny@cc.lan>
2023-12-30 22:39:59 +0800

    fix:scrip

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,13 +1,15 @@
 export default {
     title: 'Tyro Cloud',
     description: '魔法梯子科学上网.',
+    titleTemplate: '小白云',
     base: '/tyro/',
     lang: 'zh-CN',
+    ignoreDeadLinks: true,
+    outDir: '../dist',
     themeConfig: {
         siteTitle: 'Tyro Cloud · 小白云',
         crisp: '0de87b57-f520-4955-99fd-f9557fc7adc6',
         nav: [
-            { text: '', link: 'http://cloud.idid88.com/' },
             { text: '账号杂货铺', link: 'http://cloud.idid88.com/' },
             {
                 text: 'Todesk 远程',
@@ -19,11 +21,7 @@ export default {
           ]
       },
       head: [
-        [
-            'script',
-            {},
-            `window.$crisp=[];window.CRISP_WEBSITE_ID="0de87b57-f520-4955-99fd-f9557fc7adc6";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`
-          ]
+        ['link', { rel: 'icon', href: '/icon.png' }],
       ]
   }
   
