7af0d0a6fd6e7e5b5c6e982963857f57cdcb3f2c
unknown <coocangela@gmail.com>
2026-05-09 02:24:56 +0800

    up

--- docs/components/notification.vue
+++ docs/components/notification.vue
@@ -8,7 +8,7 @@
         message: '请 更新 苹果手机、iPad Digilink APP，其他设备请 断开连接，卸载 原Digilink 软件，在官网下载安装新版客户端！使用第三方客户端的用户请切换至官方客户端。',
         dangerouslyUseHTMLString: true,
         offset: 100,
-        duration: 10000,
+        duration: 20000,
       })
     }
   }
